4de61c83fcb9f2632f4bf3bce8294cd6ba02c2cb
Mani_Dev <dhingramsp@Gmail.com>
2022-08-17 19:54:38 +0530

    idk

--- BloonChipper/ModHelperData.cs
+++ BloonChipper/ModHelperData.cs
@@ -6,8 +6,7 @@
         public const string Name = "Bloon Chipper";
         public const string Description = "Added the bloonchipper in all it's glory (kinda, only 2 paths complete)";
         public const string RepoOwner = "Mani-cwaf";
-        public const string RepoName = "BTD6Mods";
-        public const string SubPath = "Mods/BloonChipper";
+        public const string RepoName = "BloonChipper";
         public const string DllName = "BloonChipper.dll";
     }
 }
